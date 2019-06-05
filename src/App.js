34089be8a7f1da311bfffae4345987293fd6842c
Yulia <duratsia@students.softuni.bg>
2019-06-05 13:53:34 +0300

    adding clarify

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import React, { Component } from 'react';
 import './App.css';
 import Particles from 'react-particles-js';
-import Clarifai from 'clarifai';
 import Navigation from './components/navigation/Navigation';
 import SignIn from './components/SignIn/SignIn';
 import Register from './components/Register/Register';
@@ -9,10 +8,6 @@ import Logo from './components/logo/Logo';
 import Rank from './components/Rank/Rank';
 import ImageLinkForm from './components/imageLinkForm/ImageLinkForm';
 import FaceRecognition from './components/FaceRecognition/FaceRecognition';
-
-const app = new Clarifai.App({
-  apiKey: 'efdd94c425bd471d9ceefd63cb863702'
- });
 
 const particlesOptions = {
   particles: {
@@ -97,10 +92,14 @@ class App extends Component {
 
   onButtonSubmit = (event) => {
     this.setState({imageUrl: this.state.input})
-    app.models
-    .predict(
-      Clarifai.FACE_DETECT_MODEL,
-      this.state.input)
+    fetch('http://localhost:3000/imageurl', {
+          method: 'post',
+          headers: {'Content-Type': 'application/json'},
+          body: JSON.stringify({
+            input: this.state.input
+          })
+        })
+        .then(response => response.json())
     .then(response => {
       if(response)  {
         fetch('http://localhost:3000/image', {
